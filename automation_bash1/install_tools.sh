37ba0d8e2e34d41e74dbab56f6af8d0c82b8fe2d
own_the_net <91747542+ALLann123@users.noreply.github.com>
2026-05-19 12:06:39 +0300

    install required tools

---
+++ automation_bash1/install_tools.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Install Go first if you haven't
+wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+# Install the core toolkit
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+go install -v github.com/projectdiscovery/katana/cmd/katana@latest
+go install -v github.com/lc/gau/v2/cmd/gau@latest
+go install -v github.com/tomnomnom/assetfinder@latest
+go install -v github.com/tomnomnom/waybackurls@latest
+go install -v github.com/tomnomnom/anew@latest
+go install -v github.com/tomnomnom/gf@latest
+go install -v github.com/tomnomnom/qsreplace@latest
+go install -v github.com/hahwul/dalfox/v2@latest
+go install -v github.com/haccer/subjack@latest
+pip3 install uro
