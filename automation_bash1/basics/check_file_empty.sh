63c11d76b889c62a3d7dcad9ef440080afadc6b1
own_the_net <91747542+ALLann123@users.noreply.github.com>
2026-05-21 11:56:08 +0300

    refresh basics

---
+++ automation_bash1/basics/check_file_empty.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# get our current working directory
+CURRENT_PATH=$(pwd)
+
+# the our target folder is ut
+TARGET_FILE="ut"
+
+NEW_PATH=$CURRENT_PATH/$TARGET_FILE
+
+echo "File exists at: $NEW_PATH"
+
+
